e855a9147a639a37caaa7ee2a206f52888c33169
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-05-02 15:51:22 -0300

    basic view now returns minimal html

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from .views import home
 
 class HomepageTest(TestCase):
@@ -7,3 +9,10 @@ class HomepageTest(TestCase):
     def test_root_url_resolves_to_home(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+
+    def test_home_returns_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Cumbia</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home():
-    pass
+def home(request):
+    return HttpResponse('<html><title>Cumbia</title></html>')
