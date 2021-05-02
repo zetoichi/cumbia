c36625f12c6f662ecaa36a3220570c7afe48353c
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-05-02 17:05:25 -0300

    first rf to use a template

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import index
 
@@ -10,9 +11,11 @@ class IndexTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, index)
 
-    def test_home_returns_correct_html(self):
+    def test_index_returns_correct_html(self):
         request = HttpRequest()
         response = index(request)
-        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
-        self.assertIn(b'<title>Cumbia</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('portfolio/index.html')
+        self.assertEqual(
+            response.content.decode(),
+            expected_html
+        )
