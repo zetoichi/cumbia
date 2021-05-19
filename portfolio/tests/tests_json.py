06646afc604995d42aad0612fb09c393ab33b92a
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-05-19 12:59:52 -0300

    rewrote tests. again.

--- portfolio/tests/tests_json.py
+++ portfolio/tests/tests_json.py
@@ -1,13 +1,3 @@
-from PIL import Image
-
-from django.core.files.base import ContentFile
-
-
-from . import tests_main
-
-from ..views_json import (
+from portfolio.views_json import (
     save_new_pics
 )
-
-# def create_mock_imgs(storage, filename,
-#     size=(1920, 1920), image_mode='RGB', image_format='PNG')
--- portfolio/tests/tests_main.py
+++ portfolio/tests/tests_main.py
@@ -4,12 +4,11 @@ from django.test import (
     TestCase,
 )
 
-from .mixins import GeneralCBVMixin
-from ..views_main import (
+from portfolio.views_main import (
     IndexView,
     PhDetailView,
 )
-from ..models import Photographer, Pic
+from portfolio.models import Photographer, Pic
 
 class GeneralCBVTestCase(TestCase):
 
@@ -20,10 +19,12 @@ class GeneralCBVTestCase(TestCase):
             first_name='First',
             last_name='Fake Ph'
         )
+        print(cls.p1)
         cls.p2 = Photographer.objects.create(
             first_name='Second',
             last_name='Fake Ph'
         )
+        print(cls.p2)
 
     @classmethod
     def tearDownClass(cls):
@@ -31,17 +32,53 @@ class GeneralCBVTestCase(TestCase):
         cls.p2.delete()
         super().tearDownClass()
 
-class IndexTest(GeneralCBVMixin, GeneralCBVTestCase):
-    url = '/'
-    view_class = IndexView
-    expected_template = 'portfolio/index.html'
+    # RESOLVERS
+
+    def test_index_url_resolves(self):
+        url = '/'
+        view_class = IndexView
+        response = self.client.get(url)
+        self.assertEqual(
+            response.resolver_match.func.__name__,
+            view_class.as_view().__name__
+        )
+
+    def test_index_uses_expected_template(self):
+        url = '/'
+        expected_template = 'portfolio/index.html'
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, expected_template)
+
+    def test_ph_detail_url_resolves(self):
+        url_1 = '/phs/1/'
+        url_2 = '/phs/2/'
+        view_class = PhDetailView
+
+        response_1 = self.client.get(url_1)
+        response_2 = self.client.get(url_2)
+        expected_1 = response_1.resolver_match.func.__name__
+        expected_2 = response_2.resolver_match.func.__name__
+        actual_1 = view_class.as_view().__name__
+        actual_2 = view_class.as_view().__name__
+        self.assertEqual(expected_1, actual_1)
+        self.assertEqual(expected_2, actual_2)
+
+    def test_ph_detail_uses_expected_template(self):
+        url_1 = '/phs/1/'
+        url_2 = '/phs/2/'
+        expected_template = 'portfolio/ph_detail.html'
+
+        response_1 = self.client.get(url_1)
+        response_2 = self.client.get(url_2)
+        self.assertTemplateUsed(response_1, expected_template)
+        self.assertTemplateUsed(response_2, expected_template)
+
+    # CONTENT
 
     def test_index_displays_photographers(self):
-        response = self.client.get('/')
-        self.assertIn('First Fake Ph', response.content.decode())
-        self.assertIn('Second Fake Ph', response.content.decode())
-
-class PhDetailTest(GeneralCBVMixin, GeneralCBVTestCase):
-    url = '/phs/3/'
-    view_class = PhDetailView
-    expected_template = 'portfolio/ph_detail.html'
+        url = '/'
+        response = self.client.get(url).content.decode()
+        expected_1 = 'First Fake Ph'
+        expected_2 = 'Second Fake Ph'
+        self.assertIn(expected_1, response)
+        self.assertIn(expected_2, response)
