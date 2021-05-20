098cbc81f73335d64086e67270bc59e2a0a5b5db
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-05-20 18:12:32 -0300

    dropzone working, default style

--- core/static/src/js/dz_conf.js
+++ core/static/src/js/dz_conf.js
@@ -1,8 +1,27 @@
-const pk = location.pathname.split('/').reverse()[0]
-
 Dropzone.options.imgDropzone = {
     method: 'post',
+    paramName: 'pic',
     autoProcessQueue: false,
     uploadMultiple: true,
     dictDefaultMessage: 'Arrastre aquí las imágenes...',
+    acceptedFiles: '.jpg,.png',
+    addRemoveLinks: true,
+    maxFiles: 12,
+    parallelUploads: 12,
+    init: function() {
+        const uploadBtn = document.querySelector('#upload-imgs-btn')
+
+        uploadBtn.addEventListener('click', e => {
+            e.stopPropagation()
+            e.preventDefault()
+            this.processQueue()
+        })
+
+        this.on('successmultiple', (files, response) => {
+            window.location.reload()
+        })
+        this.on('errormultiple', (files, response) => {
+            console.log(response)
+        })
+    }
 }
