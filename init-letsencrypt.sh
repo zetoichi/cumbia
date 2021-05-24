bd719ed939844eb929d7d226e233cbc0957f427e
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-05-24 19:36:13 -0300

    fixed letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(zetoichi-b.xyz www.zetoichi-b.xyz)
+domains=(zetoichi-b.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="zetoichi.dev@protonmail.com"
