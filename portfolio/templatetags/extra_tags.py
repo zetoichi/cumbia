9316c69731b8ab1b59276e8b089c09ff4e7123ea
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-06-04 14:39:39 -0300

    added templatetags to portfolio

--- portfolio/templatetags/extra_tags.py
+++ portfolio/templatetags/extra_tags.py
@@ -5,14 +5,9 @@ from django.forms import Form
 
 register = template.Library()
 
-@register.inclusion_tag('tags/simple_form.html')
-def simple_form(form: Type[Form], main_action: str = 'Enviar',
-            secondary_action: Optional[str] = None) -> Dict[str, Any]:
-    """
-    """
-    context = {
-        'form': form,
-        'main_action': main_action,
-        'secondary_action': secondary_action,
-    }
-    return context
+# @register.inclusion_tag('tags/simple_form.html')
+# def simple_tag() -> Dict[str, Any]:
+#     """
+#     """
+#     context = {}
+#     return context
