39ca50d68736efd63a77d0e2d9ee5e3d38ec38a1
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-06-07 15:20:22 -0300

    docker-compose modified media volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
+gunicorn core.wsgi:application --bind 0.0.0.0:8000
