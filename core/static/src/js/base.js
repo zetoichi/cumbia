033f6ce6e527961970802b59f00120c26c225451
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-06-08 12:46:44 -0300

    detail: delete and main logic (front)

--- core/static/src/js/base.js
+++ core/static/src/js/base.js
@@ -1,4 +1,4 @@
-import { togglePics } from './handlePics';
+import { handleMainToggle, handlePicActions } from './handlePics';
 
 if (document.readyState == 'loading') {document.addEventListener(
     'DOMContentLoaded', ready)
@@ -10,7 +10,8 @@ if (document.readyState == 'loading') {document.addEventListener(
 
 function ready() {
     handleEditMode()
-    handleToggle()
+    handleMainToggle()
+    handlePicActions()
 }
 
 function handleEditMode() {
@@ -26,44 +27,31 @@ function handleEditMode() {
     }
 }
 
-function handleToggle() {
-    const phLinks = document.querySelectorAll('.ph-link')
-    
-    phLinks.forEach(link => {
-        link.addEventListener('mouseenter', (e) => {
-            togglePics(e)
-        })
-    })
-    phLinks.forEach(link => {
-        link.addEventListener('mouseleave', (e) => {
-            togglePics(e)
-        })
-    })
-}
-
 function animateOpening() {
     const big = document.querySelector('.big')
     const fullScreenLogo = document.querySelector('#full-screen-logo')
     
-    setTimeout(() => {
-        fullScreenLogo.style.display = 'none'
-    }, 1500);
-    setTimeout(() => {
-        fullScreenLogo.style.display = 'inline'
-    }, 1600);
-    setTimeout(() => {
-        fullScreenLogo.style.display = 'none'
-    }, 2000);
-    setTimeout(() => {
-        fullScreenLogo.style.display = 'inline'
-    }, 2100);
-    setTimeout(() => {
-        fullScreenLogo.style.display = 'none'
-    }, 3000);
-    setTimeout(() => {
-        big.style.opacity = 0
-    }, 3800);
-    setTimeout(() => {
-        big.remove()
-    }, 5100);
+    if (big && fullScreenLogo) {
+        setTimeout(() => {
+            fullScreenLogo.style.display = 'none'
+        }, 1500);
+        setTimeout(() => {
+            fullScreenLogo.style.display = 'inline'
+        }, 1600);
+        setTimeout(() => {
+            fullScreenLogo.style.display = 'none'
+        }, 2000);
+        setTimeout(() => {
+            fullScreenLogo.style.display = 'inline'
+        }, 2100);
+        setTimeout(() => {
+            fullScreenLogo.style.display = 'none'
+        }, 3000);
+        setTimeout(() => {
+            big.style.opacity = 0
+        }, 3800);
+        setTimeout(() => {
+            big.remove()
+        }, 5100);
+    }
 }
