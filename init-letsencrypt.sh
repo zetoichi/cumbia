60cb75e0d0c5027d8862c51b6217ff1c6fc87d87
Alejandro Zapiola <alejandro.zapiola@gmail.com>
2021-11-08 14:00:37 -0300

    certbot

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(zetoichi-b.xyz)
+domains=(cumbiavs.com www.cumbiavs.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="zetoichi.dev@protonmail.com"
